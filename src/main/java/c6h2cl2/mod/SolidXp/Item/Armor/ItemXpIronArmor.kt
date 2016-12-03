08a118fdce6661eef92fe0f19ff00e7fadad058a
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-12-03 23:10:22 +0900

    エラー部分コメントアウト

--- src/main/java/c6h2cl2/mod/SolidXp/Item/Armor/ItemXpIronArmor.kt
+++ src/main/java/c6h2cl2/mod/SolidXp/Item/Armor/ItemXpIronArmor.kt
@@ -4,7 +4,9 @@ import c6h2cl2.mod.SolidXp.SolidXpRegistry
 import net.minecraft.inventory.EntityEquipmentSlot
 import net.minecraft.item.ItemArmor
 
-class ItemXpIronArmor(equipmentSlotIn: EntityEquipmentSlot) : ItemArmor(SolidXpRegistry.armorMaterialXpIron, 0, equipmentSlotIn), IXpArmor {
+/*class ItemXpIronArmor(equipmentSlotIn: EntityEquipmentSlot, armorShield: Float, shieldBonus: Float, shieldLimit: Float) : ItemArmor(SolidXpRegistry.armorMaterialXpIron, 0, equipmentSlotIn), IXpArmor {
+    override val armorShield: Float
+        get() = throw UnsupportedOperationException()
 
     override fun getArmorShield(): Float {
         return 10f
@@ -17,4 +19,8 @@ class ItemXpIronArmor(equipmentSlotIn: EntityEquipmentSlot) : ItemArmor(SolidXpR
     override fun getShieldLimit(): Float {
         return 20f
     }
+}*/
+/*class ItemXpIronArmor() :ItemArmor(SolidXpRegistry.armorMaterialXpIron,0){
+
 }
+*/
